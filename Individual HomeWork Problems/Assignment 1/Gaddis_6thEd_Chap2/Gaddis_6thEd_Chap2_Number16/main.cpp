279905f734d3ad617667eccfcda8c78c13480022
bwodhams <bpwodhams@gmail.com>
2015-06-28 15:16:31 -0700

    more
    more

---
+++ Individual HomeWork Problems/Assignment 1/Gaddis_6thEd_Chap2/Gaddis_6thEd_Chap2_Number16/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Ben Wodhams
+ *
+ * Created on June 26, 2015, 4:45 PM
+ * Purpose : Gaddis 6th Edition Chapter 2 Number 5
+ */
+
+//System Libraries
+#include <iostream>//I/O Library
+
+using namespace std;//Namespace for iostream
+ 
+//User Libraries
+ 
+//Global Constants
+
+//Function Prototypes
+ 
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    
+    //Declare variables 
+    
+    
+   //Output
+    cout<<"         *"<<endl;
+    cout<<"        ***"<<endl;
+    cout<<"       *****"<<endl;
+    cout<<"      *******"<<endl;
+    cout<<"       *****"<<endl;
+    cout<<"        ***"<<endl;
+    cout<<"         *"<<endl;
+    
+    
+     return 0;
+            
+}
